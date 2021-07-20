302ba0f637de708086a2b431f2257ae507e4132c
Caroline <dev.caroline.ribeiro@gmail.com>
2021-07-20 20:38:59 -0300

    primeiro controller

---
+++ src/main/java/app/netlify/carolineandrade/tacocloud/http_rest/HomeController.java
@@ -0,0 +1,13 @@
+package app.netlify.carolineandrade.tacocloud.http_rest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home";
+    }
+}
---
+++ src/test/java/app/netlify/carolineandrade/tacocloud/TacoCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package app.netlify.carolineandrade.tacocloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
